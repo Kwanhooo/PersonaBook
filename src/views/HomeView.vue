43aefe4b60b322a7f2281f1db304f4cc4b5fce87
Kwanhooo <kwanhooo@foxmail.com>
2024-02-01 22:12:23 +0800

    chore: 移除无关代码

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div style="background: red">
+    <h1>Hello World</h1>
+  </div>
 </template>
