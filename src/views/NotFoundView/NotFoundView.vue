a05f7084cfe1dda3e9f44dd0c5ec1da540856234
Kwanhooo <kwanhooo@foxmail.com>
2024-02-19 16:55:50 +0800

    feat: 404页面

---
+++ src/views/NotFoundView/NotFoundView.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="wrapper">
+    <div class="main">
+      <div class="fof">
+        <h1>404 NOT FOUND</h1>
+        <span class="back" @click="$router.push('/')">回到主页</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import "NotFoundView";
+</style>
