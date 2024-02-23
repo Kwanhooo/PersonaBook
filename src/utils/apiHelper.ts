aca55b9947dde3ffd33cb3e760760789b2220bc9
Kwanhooo <kwanhooo@foxmail.com>
2024-02-23 20:17:44 +0800

    doc: 注释

--- src/utils/apiHelper.ts
+++ src/utils/apiHelper.ts
@@ -1,3 +1,7 @@
+/**
+ * 将对象转换为x-www-form-urlencoded格式
+ * @param data
+ */
 export function objectToForm(data: any) {
   let ret = ''
   for (const it in data) {
--- src/utils/storeHelper.ts
+++ src/utils/storeHelper.ts
@@ -1,16 +1,27 @@
 import { useUserStore } from '@/stores/modules/userStore'
 import { UserRole } from '@/interfaces/constant/magicNumberUser'
 
+/**
+ * 判断用户是否登录
+ */
 export function isUserLoggedIn() {
   const userStore = useUserStore()
   return localStorage.getItem('token') && Object.keys(userStore.user).length > 0
 }
 
+/**
+ * 获取登录用户的角色
+ */
 export function getUserRole() {
   const userStore = useUserStore()
   return userStore.user.role
 }
 
+/**
+ * 比较用户角色权限
+ * @param requiredRole
+ * @param actualRole
+ */
 export function hasPermission(requiredRole: string, actualRole: string) {
   if (requiredRole === UserRole.GUEST) return true
   if (requiredRole === UserRole.ADMIN && actualRole === UserRole.ADMIN) return true
