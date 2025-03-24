4b06b4f0ee83dfff2a3003286ff826bfa6a982d8
Kwanhooo <kwanhooo@foxmail.com>
2025-03-24 16:18:28 +0800

    build: 更新构建

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,6 +1,8 @@
-export const backendBaseUrl = 'http://192.168.1.6:8080'
+// 系统后端
+export const backendBaseUrl = 'https://api.book.0xcafebabe.cn'
+// export const backendBaseUrl = 'http://127.0.0.1:8080'
 // export const backendBaseUrl = 'http://120.79.77.193:8080'
 // export const backendBaseUrl = 'http://e68zed.natappfree.cc'
 
-
+// 阅读器后端
 export const readerBackendUrl = 'https://reader.book.0xcafebabe.cn'
--- src/requests/user.ts
+++ src/requests/user.ts
@@ -6,7 +6,7 @@ import type { SearchParam } from '@/interfaces/SearchParam'
 const UserAPI = {
   getCollectionList: `${backendBaseUrl}/account/getCollections`,
   getComments: `${backendBaseUrl}/account/getComments`,
-  search: `${backendBaseUrl}/fileList/getFilesByKeyword2`
+  search: `${backendBaseUrl}/fileList/getFilesByKeyword`
 }
 
 export function getCollectionList(data: GetCollectionListParam) {
